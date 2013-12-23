90e18de8b1a357f0fe25b1e3a1edab6e4c6451e8
Umur Gedik <umurgdk@gmail.com>
2013-12-23 18:48:03 +0200

    proc file added and port environment setted

--- lib/chat_experiment.ex
+++ lib/chat_experiment.ex
@@ -1,25 +1,29 @@
 defmodule ChatExperiment do
-	use Application.Behaviour	
+    use Application.Behaviour   
 
-	def start(_type, _args) do
-		port = 8080
+    def start(_type, _args) do
+        dispatch = :cowboy_router.compile([
+            # {URIHost, list({URIPath, Handler, Opts})}
+            {:_, [
+                {"/socket", ChatExperiment.Handlers.ChatSocket, []},
+                {"/assets/[...]", :cowboy_static, {:priv_dir, :chat_experiment, ""}},
+                {"/", :cowboy_static, {:priv_file, :chat_experiment, "index.html"}}
+            ]}
+        ])
 
-		dispatch = :cowboy_router.compile([
-			# {URIHost, list({URIPath, Handler, Opts})}
-			{:_, [
-				{"/socket", ChatExperiment.Handlers.ChatSocket, []},
-				{"/assets/[...]", :cowboy_static, {:priv_dir, :chat_experiment, ""}},
-				{"/", :cowboy_static, {:priv_file, :chat_experiment, "index.html"}}
-			]}
-		])
+        case System.get_env("PORT") do
+            nil ->  {:ok, _} = :cowboy.start_http(:http, 100,
+                    [port: 8080],
+                    [env: [dispatch: dispatch]])
 
-		{:ok, _} = :cowboy.start_http(:http, 100,
-																	[port: port],
-																	[env: [dispatch: dispatch]])
+            port -> {:ok, _} = :cowboy.start_http(:http, 100,
+                    [port: port],
+                    [env: [dispatch: dispatch]])
+        end
 
-		IO.puts "Cowboy server running @ http://localhost:#{port}!"
-		ChatServer.start_link
-		ChatExperiment.Supervisor.start_link
-	end
-	
+        IO.puts "Cowboy server running @ http://localhost:#{port}!"
+        ChatServer.start_link
+        ChatExperiment.Supervisor.start_link
+    end
+    
 end
